397d88c84e7706837303329889d1c52f49d2bf0d
ali <itskhaloufali@gmail.com>
2022-02-28 16:06:51 +0300

    move to pug html

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const app = express()
 const path = require('path')
 const port = 3000
 
-app.set('views', path.join(__dirname, '/views'))
-
-app.set('view engine', 'html')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.sendFile('views/pages/home.html', { root: __dirname })
+  res.render('index')
 })
-app.get('/about', (req, res) => {
-  res.sendFile('views/pages/about.html', { root: __dirname })
-})
+
 app.listen(port, () => {
-  console.log(`This server works with port ${port}`)
+  console.log('work')
 })
